d13461fc9362ab5dcbf738925db736316619b009
gouki04 <gouki04@126.com>
2017-03-20 19:44:41 +0800

    add base
    add base project

---
+++ encrypt/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace encrypt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbInputFile = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnChooseInputFile = new System.Windows.Forms.Button();
+            this.tbOutputFile = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnChooseOutputFile = new System.Windows.Forms.Button();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.tbKey = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbInputFile
+            // 
+            this.tbInputFile.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbInputFile.Location = new System.Drawing.Point(95, 12);
+            this.tbInputFile.Name = "tbInputFile";
+            this.tbInputFile.Size = new System.Drawing.Size(253, 21);
+            this.tbInputFile.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "input file:";
+            // 
+            // btnChooseInputFile
+            // 
+            this.btnChooseInputFile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnChooseInputFile.Location = new System.Drawing.Point(354, 12);
+            this.btnChooseInputFile.Name = "btnChooseInputFile";
+            this.btnChooseInputFile.Size = new System.Drawing.Size(57, 23);
+            this.btnChooseInputFile.TabIndex = 2;
+            this.btnChooseInputFile.Text = "...";
+            this.btnChooseInputFile.UseVisualStyleBackColor = true;
+            this.btnChooseInputFile.Click += new System.EventHandler(this.btnChooseInputFile_Click);
+            // 
+            // tbOutputFile
+            // 
+            this.tbOutputFile.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbOutputFile.Location = new System.Drawing.Point(95, 39);
+            this.tbOutputFile.Name = "tbOutputFile";
+            this.tbOutputFile.Size = new System.Drawing.Size(253, 21);
+            this.tbOutputFile.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "output file:";
+            // 
+            // btnChooseOutputFile
+            // 
+            this.btnChooseOutputFile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnChooseOutputFile.Location = new System.Drawing.Point(354, 39);
+            this.btnChooseOutputFile.Name = "btnChooseOutputFile";
+            this.btnChooseOutputFile.Size = new System.Drawing.Size(57, 23);
+            this.btnChooseOutputFile.TabIndex = 2;
+            this.btnChooseOutputFile.Text = "...";
+            this.btnChooseOutputFile.UseVisualStyleBackColor = true;
+            this.btnChooseOutputFile.Click += new System.EventHandler(this.btnChooseOutputFile_Click);
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnEncrypt.Location = new System.Drawing.Point(14, 91);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(191, 55);
+            this.btnEncrypt.TabIndex = 3;
+            this.btnEncrypt.Text = "encrypt";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDecrypt.Location = new System.Drawing.Point(227, 91);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(184, 55);
+            this.btnDecrypt.TabIndex = 3;
+            this.btnDecrypt.Text = "decrypt";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // tbKey
+            // 
+            this.tbKey.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbKey.Location = new System.Drawing.Point(95, 64);
+            this.tbKey.Name = "tbKey";
+            this.tbKey.Size = new System.Drawing.Size(253, 21);
+            this.tbKey.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "key:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(423, 158);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.btnChooseOutputFile);
+            this.Controls.Add(this.btnChooseInputFile);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbKey);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbOutputFile);
+            this.Controls.Add(this.tbInputFile);
+            this.Name = "Form1";
+            this.Text = "do encrypt";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbInputFile;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnChooseInputFile;
+        private System.Windows.Forms.TextBox tbOutputFile;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnChooseOutputFile;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.TextBox tbKey;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ encrypt/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security;
+using System.Security.Cryptography;
+using System.Runtime.Serialization;
+using System.IO;
+
+namespace encrypt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private bool check_param()
+        {
+            if (tbInputFile.Text.Length <= 0)
+            {
+                MessageBox.Show("InputFile is empty!");
+                return false;
+            }
+
+            if (!File.Exists(tbInputFile.Text))
+            {
+                MessageBox.Show("Can not find InputFile!");
+                return false;
+            }
+
+            if (tbOutputFile.Text.Length <= 0)
+            {
+                MessageBox.Show("OutputFile is empty!");
+                return false;
+            }
+
+            if (tbKey.Text.Length <= 0)
+            {
+                MessageBox.Show("Key is empty!");
+                return false;
+            }
+
+            return true;
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            if (!check_param())
+            {
+                return;
+            }
+
+            byte[] input_bytes = null;
+            using (var fs = new FileStream(tbInputFile.Text, FileMode.Open))
+            {
+                input_bytes = new byte[fs.Length];
+                fs.Read(input_bytes, 0, (int)fs.Length);
+            }
+
+            var ms = new MemoryStream();
+            var key = Encoding.UTF8.GetBytes(tbKey.Text);
+            byte[] iv = { 0x12, 0x34, 0x56, 0x78, 0x90, 0xAB, 0xCD, 0xEF };
+
+            var provider = new DESCryptoServiceProvider();
+            var cs = new CryptoStream(ms, provider.CreateEncryptor(key, iv), CryptoStreamMode.Write);
+
+            try
+            {
+                cs.Write(input_bytes, 0, input_bytes.Length);
+                cs.FlushFinalBlock();
+
+                using (var fs = new FileStream(tbOutputFile.Text, FileMode.OpenOrCreate))
+                {
+                    fs.Write(ms.ToArray(), 0, (int)ms.Length);
+                }
+            }
+            finally
+            {
+                cs.Close();
+                ms.Close();
+            }
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            if (!check_param())
+            {
+                return;
+            }
+
+            byte[] input_bytes = null;
+            using (var fs = new FileStream(tbInputFile.Text, FileMode.Open))
+            {
+                input_bytes = new byte[fs.Length];
+                fs.Read(input_bytes, 0, (int)fs.Length);
+            }
+
+            var ms = new MemoryStream();
+            var key = Encoding.UTF8.GetBytes(tbKey.Text);
+            byte[] iv = { 0x12, 0x34, 0x56, 0x78, 0x90, 0xAB, 0xCD, 0xEF };
+
+            var provider = new DESCryptoServiceProvider();
+            var cs = new CryptoStream(ms, provider.CreateDecryptor(key, iv), CryptoStreamMode.Write);
+
+            try
+            {
+                cs.Write(input_bytes, 0, input_bytes.Length);
+                cs.FlushFinalBlock();
+
+                using (var fs = new FileStream(tbOutputFile.Text, FileMode.OpenOrCreate))
+                {
+                    fs.Write(ms.ToArray(), 0, (int)ms.Length);
+                }
+            }
+            finally
+            {
+                cs.Close();
+                ms.Close();
+            }
+        }
+
+        private void btnChooseInputFile_Click(object sender, EventArgs e)
+        {
+            using (var fd = new OpenFileDialog())
+            {
+                if (fd.ShowDialog() == DialogResult.OK)
+                {
+                    tbInputFile.Text = fd.FileName;
+                }
+            }
+        }
+
+        private void btnChooseOutputFile_Click(object sender, EventArgs e)
+        {
+            using (var fd = new OpenFileDialog())
+            {
+                if (fd.ShowDialog() == DialogResult.OK)
+                {
+                    tbOutputFile.Text = fd.FileName;
+                }
+            }
+        }
+    }
+}
